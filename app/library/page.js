53852a831bd04aa6c5f37706a137b2805e720fec
ditzzzXZ <ditzzz.ofc@gmail.com>
2025-09-09 17:56:14 +0700

    Create page.js

---
+++ app/library/page.js
@@ -0,0 +1,8 @@
+export default function Library() {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold mb-6">Your Library 📚</h1>
+      <p>Coming soon: Playlist kamu di sini! 😎</p>
+    </div>
+  );
+}
